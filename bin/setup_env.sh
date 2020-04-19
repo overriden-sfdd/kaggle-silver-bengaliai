5fe837f3b590671c802b8d69063b25038a019be2
overriden-sfdd <lovejapantoomuch@gmail.com>
2020-04-19 07:44:13 +0300

    setup_env.sh fixed

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
 setup(){
     model_name=$1
-    declare -a input_folders=($2)
-    
+    input_folders=$2
     mkdir input
-    for sub_folder in "input_folders[@]" 
+    for sub_folder in $input_folders
     do
         mkdir input/$sub_folder
     done
     
     mkdir model/$model_name
 }
-
-setup xresnet50 "begnaliai-cv19" "folded_data" "train_images"
+setup xresnet50 $"bengaliai-cv19 folded_data train_images"
